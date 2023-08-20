de69d0092eb7cc4196796b1ad47e572193ee48c5
alivasisari <alivasisari1380@gmail.com>
2023-08-21 00:46:08 +0330

    Add session to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . import models
+from django.contrib.sessions.models import Session
 
 # Register your models here.
 
+admin.site.register(Session)
 admin.site.register(models.User)
