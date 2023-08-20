687c53c815a039bfe0a7f0d85c21ad0da92e9cfa
alivasisari <alivasisari1380@gmail.com>
2023-08-21 00:43:39 +0330

    Add blog model and admin

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from accounts.models import User
 
 # Catagory model
 class Catagory(models.Model):
     name = models.CharField(max_length=200, null=True)
     slug = models.SlugField(null=True)
-    image = models.CharField(max_length=300, null=True, blank=True )
     description = models.CharField(max_length=500, null=True,blank=True, verbose_name='Description')
 
     def __str__(self):
@@ -19,12 +19,12 @@ class Catagory(models.Model):
 
 # posts model
 class Post(models.Model):
-    title = models.CharField(max_length=200, max_length=200, unique=True)
-    body = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    body = models.TextField()
     slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='url title')
     category = models.ManyToManyField(Catagory, related_name='post_categories', verbose_name='categories')
-    tags = TaggableManager()
+    tags = TaggableManager(related_name='tags')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -38,10 +38,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'created by {} at {}'.format(self.owner, self.created)
