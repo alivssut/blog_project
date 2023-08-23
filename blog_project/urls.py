283deeb23427495807602e2b7804228b689bcb26
alivasisari <alivasisari1380@gmail.com>
2023-08-23 22:46:03 +0330

    Add schema urls

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,11 @@ urlpatterns = [
     path("api/v1/auth/", include("dj_rest_auth.urls")),
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/v1/", include("blog.urls")),
+    
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(
+        url_name="schema"), name="redoc",),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
+        url_name="schema"), name="swagger-ui"),
+    
 ]
