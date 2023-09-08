889d3055d899a25eec1df91a01d99d28b8556c82
alivasisari <alivasisari1380@gmail.com>
2023-09-08 13:13:05 +0330

    Add gender to register serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
-
+from .models import UserProfile, User
 
 class CustomRegisterSerializer(RegisterSerializer):
     first_name = serializers.CharField()
     last_name = serializers.CharField()
+    gender = serializers.ChoiceField(choices=User.Gender.choices)
 
     def get_cleaned_data(self):
-        super(CustomRegisterSerializer, self).get_cleaned_data()
-        return {
-            'username': self.validated_data.get('username', ''),
-            'password1': self.validated_data.get('password1', ''),
-            'password2': self.validated_data.get('password2', ''),
-            'email': self.validated_data.get('email', ''),
-            'first_name': self.validated_data.get('first_name', ''),
-            'last_name': self.validated_data.get('last_name', '')
-        }
+        cleaned_data = super(CustomRegisterSerializer, self).get_cleaned_data()
+        # print(self.validated_data.get('gender',''))
+        cleaned_data['first_name'] = self.validated_data.get('first_name','')
+        cleaned_data['last_name'] = self.validated_data.get('last_name','')
+        cleaned_data['gender'] = self.validated_data.get('gender','')
+        print(cleaned_data)
+        return cleaned_data
+        
+    def save(self, *args, **kwargs):
+       user = super(CustomRegisterSerializer, self).save(*args, **kwargs) # Call the real save() method
+       user.gender = self.validated_data.get('gender','')
+       user.save()
+       UserProfile.objects.create(user=user)
+       return user
+       
