b58c072ef6e75763cee57c8f7e0721442967d65a
alivasisari <alivasisari1380@gmail.com>
2023-09-15 14:09:42 +0330

    Change search viewset

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import SearchView
 
 urlpatterns = [
-    path("search/", SearchView.as_view(), name="search_posts"),
-    # path("search/", SearchView.as_view({'get':'retrieve'}), name="search_posts"),
+    path("search/", SearchView.as_view({'get':'list'}), name="search_posts"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,20 +1,99 @@
 from rest_framework import generics
 from blog.documents import PostDocument
-from blog.serializers import PostDocumentSerializer, PostSerializer
-from django.http import HttpResponse
-from blog.models import Post
+from blog.serializers import PostDocumentSerializer
 from elasticsearch_dsl import Q
 from rest_framework.permissions import AllowAny
-from blog.pagination import PostPagination
     
-class SearchView(generics.ListAPIView):
-    pagination_class = PostPagination
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    OrderingFilterBackend,
+    SearchFilterBackend,
+    SuggesterFilterBackend,
+    DefaultOrderingFilterBackend,   
+)
+from django_elasticsearch_dsl_drf.constants import (
+    LOOKUP_FILTER_RANGE,
+    LOOKUP_QUERY_GT,
+    LOOKUP_QUERY_GTE,
+    LOOKUP_QUERY_IN,
+    LOOKUP_QUERY_LT,
+    LOOKUP_QUERY_LTE,
+    SUGGESTER_COMPLETION,
+)
+
+class SearchView(DocumentViewSet):
+    document = PostDocument
     serializer_class = PostDocumentSerializer
-    permission_classes = [AllowAny,]
+    fielddata=True
+    filter_backends = [
+        DefaultOrderingFilterBackend,
+        FilteringFilterBackend,
+        SearchFilterBackend,
+        SuggesterFilterBackend,
+        OrderingFilterBackend,
+    ]
+    
+    search_fields = (
+        'title',
+        'body',
+    )
+    multi_match_search_fields = (
+        'title',
+        'body',
+    )
+    filter_fields = {
+       'title' : {
+        'field' : 'title.raw',
+        'lookups': [
+                LOOKUP_QUERY_IN,
+                LOOKUP_QUERY_GT,
+                LOOKUP_QUERY_GTE,
+                LOOKUP_QUERY_LT,
+                LOOKUP_QUERY_LTE,
+            ],
+
+        },
+        'body' : {
+        'field' : 'body.raw',
+        'lookups': [
+                LOOKUP_FILTER_RANGE,
+                LOOKUP_QUERY_IN,
+                LOOKUP_QUERY_GT,
+                LOOKUP_QUERY_GTE,
+                LOOKUP_QUERY_LT,
+                LOOKUP_QUERY_LTE,
+            ],
+
+        },
+    }
+    suggester_fields = {
+        'title_suggest': {
+            'field': 'title.suggest',
+            'suggesters': [
+                SUGGESTER_COMPLETION,
+            ],
+            'options': {
+                'size': 20,
+                'skip_duplicates': True,
+            },
+        },
+        'body_suggest': {
+            'field': 'body.suggest',
+            'suggesters': [
+                SUGGESTER_COMPLETION,
+            ]
+        }
+    }
+    ordering_fields = {
+        'id': None,
+    }
     
     def get_queryset(self):
-        q = self.request.GET.get('q')
-        q = f'*{q}*'
-        query = Q("wildcard", title=q) | Q("wildcard", body=q)
-        search = PostDocument.search().query(query).to_queryset()
-        return search
+        qs = super().get_queryset()
+        search_query  = self.request.query_params.get('q', None)
+        if search_query:
+            search_query  = f'*{search_query}*'
+            query = Q("wildcard", title=search_query ) | Q("wildcard", body=search_query )            
+            qs = qs.query(query)
+        return qs
