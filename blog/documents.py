077c3db6d3c33331422d6351704e727bcb2e685b
alivasisari <alivasisari1380@gmail.com>
2023-09-15 14:10:18 +0330

    Change documents.py

--- blog/documents.py
+++ blog/documents.py
@@ -1,10 +1,30 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from .models import Post
+from elasticsearch_dsl import analyzer, tokenizer
 
+autocomplete_analyzer = analyzer('autocomplete_analyzer',
+            tokenizer=tokenizer('trigram', 'nGram', min_gram=1, max_gram=20),
+            filter=['lowercase']
+        )
 
 @registry.register_document
 class PostDocument(Document):
+    title = fields.TextField(
+        attr='title',
+        fields={
+            'raw': fields.TextField(required=True,analyzer=autocomplete_analyzer),
+            'suggest': fields.CompletionField(),
+            
+        }
+    )
+    body = fields.TextField(
+        attr='body',
+        fields={
+            'raw': fields.TextField(required=True,analyzer=autocomplete_analyzer),
+            'suggest': fields.CompletionField()
+        }
+    )
     class Index:
         name = 'posts'    
         settings = {
@@ -13,7 +33,3 @@ class PostDocument(Document):
         }    
     class Django:
         model = Post        
-        fields = [
-            'title',
-            'body',
-        ]
