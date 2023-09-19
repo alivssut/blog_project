cccd26cede351d73cd43c397de45e16be24ea5cc
alivasisari <alivasisari1380@gmail.com>
2023-09-19 19:19:45 +0330

    Add admin views and urls

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, PostCommentView
+from .views import CommentDetailView, CommentUpdateView, CommentDeleteView, CommentCreateView, CommentListView
+from .views import CatagoryListView, CatagoryPostsListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+from .views import TagListView, TagPostsListView
 
 urlpatterns = [
     path("admin-panel/posts/<slug:slug>/", PostDetailView.as_view(), name="admin_panel_post_detail"),
     path("admin-panel/posts/<int:pk>/update/", PostUpdateView.as_view(), name="admin_panel_post_update"),
     path("admin-panel/posts/<int:pk>/delete/", PostDeleteView.as_view(), name="admin_panel_post_delete"),
     path("admin-panel/posts/create/", PostCreateView.as_view(), name="admin_panel_post_create"),
-    path("admin-panel/posts/", PostListView.as_view(), name="admin_panel_post_list"),    
+    path("admin-panel/posts/", PostListView.as_view(), name="admin_panel_post_list"),
+    path("admin-panel/posts/<slug:slug>/comments/", PostCommentView.as_view(), name="admin_panel_post_comments"),
+    
+    path("admin-panel/comments/<int:pk>/", CommentDetailView.as_view(), name="admin_panel_comment_detail"),
+    path("admin-panel/comments/<int:pk>/update/", CommentUpdateView.as_view(), name="admin_panel_comment_update"),
+    path("admin-panel/comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="admin_panel_comment_delete"),
+    path("admin-panel/comments/create/", CommentCreateView.as_view(), name="admin_panel_comment_create"),
+    path("admin-panel/comments/", CommentListView.as_view(), name="admin_panel_comment_list"),
+    
+    path("admin-panel/tags/", TagListView.as_view(), name="admin_panel_tag_list"),
+    path("admin-panel/tags/<slug:slug>/posts/", TagPostsListView.as_view(), name="admin_panel_tag_posts"),
+    
+    path("admin-panel/categories/", CatagoryListView.as_view(), name="admin_panel_category_list"),
+    path("admin-panel/categories/<slug:slug>/posts/", CatagoryPostsListView.as_view(), name="admin_panel_category_posts"),
+    path("admin-panel/categories/create/", CategoryCreateView.as_view(), name="admin_panel_category_create"),
+    path("admin-panel/categories/<int:pk>/update/", CategoryUpdateView.as_view(), name="admin_panel_category_update"),
+    path("admin-panel/categories/<int:pk>/delete/", CategoryDeleteView.as_view(), name="admin_panel_category_delete"),
+    
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from blog.models import Post, Comment, Catagory
 from blog.serializers import PostSerializer, CommentSerializer, CategorySerializer
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAdminUser
 from blog.pagination import PostPagination, CommentPagination
 from taggit.models import Tag, TaggedItem
 from blog.serializers import MyTagSerializer
@@ -10,7 +10,7 @@ from blog.permissions import IsOwnerPermission
 # post list view
 class PostListView(generics.ListAPIView):
     pagination_class = PostPagination
-    queryset = Post.objects.all()
+    queryset = Post.objects.all_posts(active=False)
     serializer_class = PostSerializer
     permission_classes = [IsAdminUser,]
     
@@ -25,15 +25,116 @@ class PostDetailView(generics.RetrieveAPIView):
     permission_classes = [IsAdminUser,]
     lookup_url_kwarg  = 'slug'
     lookup_field = 'slug'
-    queryset = Post.objects.all()
+    queryset = Post.objects.all_posts(active=False)
     
 # post update view
 class PostUpdateView(generics.UpdateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAdminUser,]
-    queryset = Post.objects.all()
+    queryset = Post.objects.all_posts(active=True)
     
 # post delete view
 class PostDeleteView(generics.DestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAdminUser,]
+    
+
+########################### comments ##########################
+
+# Comment list view
+class CommentListView(generics.ListAPIView):
+    pagination_class = CommentPagination
+    queryset = Comment.objects.all_comments(active=False)
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminUser,]
+    
+# Comment create view
+class CommentCreateView(generics.CreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminUser]
+
+# Comment detail view
+class CommentDetailView(generics.RetrieveAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminUser,]
+    
+# Comment update view
+class CommentUpdateView(generics.UpdateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerPermission,]
+    queryset = Comment.objects.all_comments(active=False)
+    
+# Comment delete view
+class CommentDeleteView(generics.DestroyAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerPermission,]
+
+# post comments view
+class PostCommentView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminUser,]
+    
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        comments = Comment.objects.all_related_comments_to_post(post_slug=slug)
+        return comments
+    
+
+########################### category ##########################
+
+
+# categories
+class CatagoryListView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser,]
+    queryset = Catagory.objects.all()
+
+
+# category posts view
+class CatagoryPostsListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAdminUser,]
+    lookup_url_kwarg  = 'slug'
+    
+    def get_queryset(self):
+        slug = self.kwargs.get(self.lookup_url_kwarg)
+        queryset = Post.objects.all_related_posts_to_category(category_slug=slug)
+        return queryset
+    
+# category delete view
+class CategoryDeleteView(generics.DestroyAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser,]
+    
+# category create view
+class CategoryCreateView(generics.CreateAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser,]
+    
+# category update view
+class CategoryUpdateView(generics.UpdateAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser,]
+    queryset = Catagory.objects.all()
+    
+    
+########################### tag ##########################
+    
+
+# tag list view
+class TagListView(generics.ListAPIView):
+    serializer_class = MyTagSerializer
+    permission_classes = [IsAdminUser,]
+    queryset = Tag.objects.all()
+
+
+# tag posts view
+class TagPostsListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAdminUser,]
+    lookup_url_kwarg  = 'slug'
+    
+    def get_queryset(self):
+        slug = self.kwargs.get(self.lookup_url_kwarg)
+        queryset = Post.objects.all_related_posts_to_tag(tag_slug=slug)
+        return queryset
