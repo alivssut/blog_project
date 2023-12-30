73ef040c08c1f8cad213690daa5844f91fa26fa7
alivasisari <alivasisari1380@gmail.com>
2023-12-30 21:04:43 +0330

    Add base home page and navbar and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Footer from './components/footer/footer'
+import NavigationBar from './components/navBar/NavigationBar'
+import HomePage from './pages/homePage'
+import PostList from './components/posts/postList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar />
+        <HomePage />
+        <PostList />
+        {/* <header className="App-header">
+        
+        </header> */}
+        {/* <Switch> */}
+          {/* <Route exact path="/" component={Body} /> */}
+        {/* </Switch> */}
+        <Footer />
+        {/* <Body/> */}
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/homePage.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect, useCallback, useMemo} from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap'
+// import './NavigationBar.css'
+import {Link} from 'react-router-dom'
+
+function HomePage() {
+  return (
+	<div>
+        
+    </div>
+  )
+}
+
+export default HomePage
