00d39e4e15fc9c00d6f13a43b1a41537a7add670
alivasisari <alivasisari1380@gmail.com>
2024-01-05 13:04:30 +0330

    Add token auth for blog views

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -6,22 +6,26 @@ from .pagination import PostPagination, CommentPagination
 from taggit.models import Tag, TaggedItem
 from .serializers import MyTagSerializer
 from .permissions import IsOwnerPermission
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 # post list view
 class PostListView(generics.ListAPIView):
     pagination_class = PostPagination
     queryset = Post.objects.all_posts(active=True)
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     
 # post create view
 class PostCreateView(generics.CreateAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated,]
 
 # post detail view
 class PostDetailView(generics.RetrieveAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     lookup_url_kwarg  = 'slug'
     lookup_field = 'slug'
@@ -29,6 +33,7 @@ class PostDetailView(generics.RetrieveAPIView):
     
 # post update view
 class PostUpdateView(generics.UpdateAPIView):
+    authentication_classes = [TokenAuthentication]
     serializer_class = PostSerializer
     permission_classes = [IsOwnerPermission,]
     queryset = Post.objects.all_posts(active=True)
@@ -36,6 +41,7 @@ class PostUpdateView(generics.UpdateAPIView):
 # post delete view
 class PostDeleteView(generics.DestroyAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsOwnerPermission,]
 
 # Comment list view
@@ -43,32 +49,38 @@ class CommentListView(generics.ListAPIView):
     pagination_class = CommentPagination
     queryset = Comment.objects.all_comments(active=True)
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     
 # Comment create view
 class CommentCreateView(generics.CreateAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
 # Comment detail view
 class CommentDetailView(generics.RetrieveAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     
 # Comment update view
 class CommentUpdateView(generics.UpdateAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsOwnerPermission,]
     queryset = Comment.objects.all_comments(active=True)
     
 # Comment delete view
 class CommentDeleteView(generics.DestroyAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsOwnerPermission,]
 
 # post comments view
 class PostCommentView(generics.ListAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     
     def get_queryset(self):
@@ -80,6 +92,7 @@ class PostCommentView(generics.ListAPIView):
 # tag list view
 class TagListView(generics.ListAPIView):
     serializer_class = MyTagSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     queryset = Tag.objects.all()
 
@@ -87,6 +100,7 @@ class TagListView(generics.ListAPIView):
 # tag posts view
 class TagPostsListView(generics.ListAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     lookup_url_kwarg  = 'slug'
     
@@ -98,6 +112,7 @@ class TagPostsListView(generics.ListAPIView):
 # categories
 class CatagoryListView(generics.ListAPIView):
     serializer_class = CategorySerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     queryset = Catagory.objects.all()
 
@@ -105,6 +120,7 @@ class CatagoryListView(generics.ListAPIView):
 # category posts view
 class CatagoryPostsListView(generics.ListAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny,]
     lookup_url_kwarg  = 'slug'
     
