6dd2d1c1a6f210d7eb88cb30b933ba2c311bbde8
alivasisari <alivasisari1380@gmail.com>
2024-01-10 22:30:05 +0330

    Set default auth class to token

--- backend/blog_project/settings.py
+++ backend/blog_project/settings.py
@@ -217,7 +217,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
+
 
 # Schema
 
