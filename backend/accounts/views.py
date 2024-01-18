9cf29780d53d71193cf6f89ef0f9450bec71d2a4
alivasisari <alivasisari1380@gmail.com>
2024-01-18 15:07:51 +0330

    Add userdetail api field username, fname, lname, joined_date

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -12,7 +12,7 @@ class UserDetail(APIView):
     
     def get(self, request, user_id):
         
-        user = UserProfile.objects.filter(user_id=user_id).select_related().values("user_id", "avatar", "about_user", email=F("user__email") , user_type=F("user__user_type"))
+        user = UserProfile.objects.filter(user_id=user_id).select_related().values("user_id", "avatar", "about_user", email=F("user__email") , user_type=F("user__user_type"), username=F("user__username"), first_name=F("user__first_name"), last_name=F("user__last_name"), date_joined=F("user__date_joined"))
         if not user.exists():
             raise Http404
         return Response(user[0], status=status.HTTP_200_OK)
